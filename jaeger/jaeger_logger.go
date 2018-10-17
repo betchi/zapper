7bd0586d6a66069f9508fd9a25e48cf8e07f9d1b
betchi <shinichi.minobe@gmail.com>
2018-10-17 18:46:12 +0900

    Add noop flag to jaeger and elasticapm logger

--- jaeger/jaeger_logger.go
+++ jaeger/jaeger_logger.go
@@ -6,19 +6,33 @@ import (
 	"github.com/betchi/zapper"
 )
 
+// Config is settings of jaeger logger
+type Config struct {
+	Noop bool
+}
+
 // Logger is a logger for gorp
-type Logger struct{}
+type Logger struct {
+	Noop bool
+}
 
-var logger = &Logger{}
+var logger *Logger
 
-// GlobalLogger retrieve global logger for elastic apm
+// InitGlobalLogger initialize global logger for jaeger
+func InitGlobalLogger(config *Config) {
+	logger = &Logger{
+		Noop: config.Noop,
+	}
+}
+
+// GlobalLogger retrieve global logger for jaeger
 func GlobalLogger() *Logger {
 	return logger
 }
 
 // Error logs a message at ErrorLevel for jaeger
 func (l *Logger) Error(msg string) {
-	if l == nil {
+	if l == nil || l.Noop {
 		return
 	}
 
@@ -27,7 +41,7 @@ func (l *Logger) Error(msg string) {
 
 // Infof logs a message at InfoLevel for jaeger
 func (l *Logger) Infof(msg string, args ...interface{}) {
-	if l == nil {
+	if l == nil || l.Noop {
 		return
 	}
 
